4576ac65958f3beb6972881b5197a6d85179e67d
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2022-02-11 17:06:35 +0100

    interblock dilatation left works

--- testAll.cu
+++ testAll.cu
@@ -152,8 +152,8 @@ extern "C" inline void testMainPasswes() {
 	//setArrCPU(arrGoldObj, 2, 2, 31 , 2);//
 	//setArrCPU(arrGoldObj, 2, 2, 40 , 2);//
 
-	setArrCPU(arrGoldObj, 35, 0, 0, 2);//
-	setArrCPU(arrGoldObj, 31, 0, 0, 2);//
+	setArrCPU(arrGoldObj, 32, 0, 0, 2);//
+	setArrCPU(arrGoldObj, 0, 0, 0, 2);//
 
 
 
